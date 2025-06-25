6fb5097d0a75e59e1347c2729ca13d87bc4db51d
Alexayala07 <alexayaladom2002@gmail.com>
2025-06-25 11:39:38 -0600

    zip.js

--- zip.js
+++ zip.js
@@ -11,10 +11,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const inputNombreManual = document.getElementById("nombreManual");
 
   const origen = localStorage.getItem("origen") || "documentacion-general.html";
-  const docsEmpresa = JSON.parse(localStorage.getItem("scannedDocsEmpresa") || "{}");
-  const docsGeneral = JSON.parse(localStorage.getItem("scannedDocsGeneral") || "{}");
-  const imagenes = Object.keys(docsEmpresa).length > 0 ? docsEmpresa : docsGeneral;
-
+  const imagenes = JSON.parse(localStorage.getItem(
+    origen.includes("empresa") ? "scannedDocsEmpresa" : "scannedDocsGeneral"
+  ) || "{}");
 
   const posiblesDocs = ["ine_frente", "curp", "contrato_laboral", "carta_responsiva"];
   let zipBlob = null;
